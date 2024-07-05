34fc0226d29f93d01d7a528109cbe3ef7ca83480
jawadmehmood <jawadam5678@gmail.com>
2024-07-06 02:57:48 +0500

    completed

---
+++ app.js
@@ -0,0 +1,9 @@
+var fullName = "Jhone Doe";
+var age = "15 years old";
+var profession = "Certified Mobile Application Developer";
+
+alert(fullName);
+alert(age);
+alert(profession);
+
+console.log(fullName , age , profession);
